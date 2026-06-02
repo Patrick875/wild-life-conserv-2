f6da168a55ac737979f14e1c7bdd50b1aa100e7b
Patrick875 <kubwimana.ap.ed@gmail.com>
2026-06-02 16:22:09 +0200

    add a seeder cmd after db env fix-10

--- database/baseModel.py
+++ database/baseModel.py
@@ -1,6 +1,10 @@
-from datetime import datetime, timezone
+from datetime import datetime,UTC
 
 from extensions import db
+
+
+def utc_timestamp_ms():
+    return int(datetime.now(UTC).timestamp() * 1000)
 
 class BaseModel(db.Model):
     __abstract__ = True
@@ -8,17 +12,15 @@ class BaseModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     created_at = db.Column(
-        db.DateTime(timezone=True),
-        default=lambda: datetime.now(timezone.utc),
-        server_default=db.func.now(),
+        db.BigInteger,
+        default=utc_timestamp_ms,
         nullable=False,
     )
 
     updated_at = db.Column(
-        db.DateTime(timezone=True),
-        default=lambda: datetime.now(timezone.utc),
-        server_default=db.func.now(),
-        onupdate=lambda: datetime.now(timezone.utc),
+        db.BigInteger,
+        default=utc_timestamp_ms,
+        onupdate=utc_timestamp_ms,
         nullable=False,
     )
 
