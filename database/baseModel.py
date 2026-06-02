0d28550e96916496c066732cb2cad05192f00dc4
Patrick875 <kubwimana.ap.ed@gmail.com>
2026-06-02 16:03:58 +0200

    add a seeder cmd after db env fix-8

--- database/baseModel.py
+++ database/baseModel.py
@@ -8,16 +8,16 @@ class BaseModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     created_at = db.Column(
-        db.DateTime(timezone=True),
-        default=lambda: datetime.now(timezone.utc),
-        server_default=db.func.now(),
+        db.BigInteger,
+        default=lambda: datetime.now().timestamp(),
+        server_default=db.func.now().timestamp(),
         nullable=False,
     )
 
     updated_at = db.Column(
-        db.DateTime(timezone=True),
+        db.BigInteger,
         default=lambda: datetime.now(timezone.utc),
-        server_default=db.func.now(),
+        server_default=db.func.now().timestamp(),
         onupdate=lambda: datetime.now(timezone.utc),
         nullable=False,
     )
